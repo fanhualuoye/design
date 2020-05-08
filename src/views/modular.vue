fc33aa60f20eb8f823540d61a909ae55505a7674
LWJ <1028596416@qqcom>
2020-05-08 23:41:48 +0800

    first commit

---
+++ src/views/modular.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+export default {
+    name: 'modular'
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/the-constructor.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>构造器测试页面</div>
+</template>
+
+<script>
+export default {
+    name: 'the-constructor'
+}
+</script>
+
+<style scoped>
+
+</style>
