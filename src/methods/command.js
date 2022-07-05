77e642ecb4b0042f32b61f2dd64302730a26b7bc
fanhualuoye <33181883+fanhualuoye@users.noreply.github.com>
2022-07-05 17:05:11 +0800

    Update command.js

--- src/methods/command.js
+++ src/methods/command.js
@@ -1,54 +1,35 @@
-// 命令模式
-const setCommand = function (button, command) {
-    button.onclick = function () {
-        command.execute()
+const enterAccountCommand = {
+    execute: function () {
+        console.log('输入账号');
+    }
+};
+const inputPasswordCommand = {
+    execute: function () {
+        console.log('输入密码');
+    }
+};
+const signInCommand = {
+    execute: function () {
+        console.log('登录');
     }
 }
-// 具体的命令实现
-const MenuBar = {
-    refresh: function () {
-        console.log('刷新菜单目录')
+const MacroCommand = function () {
+    return {
+        commandsList: [],
+        add: function (command) {
+            this.commandsList.push(command);
+        },
+        execute: function () {
+            for (let i = 0; this.commandsList.length > i; i++) {
+                const command = this.commandsList[i]
+                command.execute()
+            }
+        }
     }
-}
-// 具体的命令实现
-const SubMenu = {
-    add: function () {
-        console.log('增加子菜单')
-    },
-    del: function () {
-        console.log('删除子菜单')
-    }
-}
-//在让 button 变得有用起来之前，我们要先把这些行为都封装在命令类中：
-const RefreshMenuBarCommand = function (receiver) {
-    this.receiver = receiver
-}
-RefreshMenuBarCommand.prototype.execute = function () {
-    this.receiver.refresh()
-}
-const AddSubMenuCommand = function (receiver) {
-    this.receiver = receiver
-}
-AddSubMenuCommand.prototype.execute = function () {
-    this.receiver.add()
-}
-const DelSubMenuCommand = function (receiver) {
-    this.receiver = receiver
-}
-DelSubMenuCommand.prototype.execute = function () {
-    this.receiver.del()
-}
-//最后就是把命令接收者传入到 command 对象中，并且把 command 对象安装到 button 上面：
-const refreshMenuBarCommand = new RefreshMenuBarCommand(MenuBar)
-const addSubMenuCommand = new AddSubMenuCommand(SubMenu)
-const delSubMenuCommand = new DelSubMenuCommand(SubMenu)
-// 命令集合
-const typeList = {
-    add: addSubMenuCommand,
-    del: delSubMenuCommand,
-    refresh: refreshMenuBarCommand
-}
-// 暴露命令入口，接收按钮和命令类型
-export default (button, command) => {
-    setCommand(button, typeList[command])
-}
+};
+
+const macroCommand = MacroCommand();
+macroCommand.add(enterAccountCommand);
+macroCommand.add(inputPasswordCommand);
+macroCommand.add(signInCommand);
+macroCommand.execute();
